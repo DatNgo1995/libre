5c11f7cd545cb1bdb7a2f498daae277d7147b590
datngo1995 <datngotat1995@gmail.com>
2019-04-03 13:32:52 +0300

    no message

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,11 +9,7 @@ import data from "./data.json";
 import { Header } from "./components/Header";
 
 class App extends Component {
-  constructor(){
-    super()
-    this.innerWidth = window.innerWidth;
 
-  }
   state = {
     data: data,
     curentTab: "stats",
